ce135f1c8b85b20afdc63ba0e4b8ad04dc7c9c12
David <sm307560@gmail.com>
2024-08-11 22:44:43 -0500

    update vite base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/colombia_dash/',
+  base: '/colombia_dash',
 })
